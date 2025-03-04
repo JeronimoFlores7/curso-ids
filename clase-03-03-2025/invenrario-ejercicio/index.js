08cfebf562ebf0cdfad5bcf57f2e942b2d507ba8
Jeronimo Gonzalez <136852043+JeronimoFlores7@users.noreply.github.com>
2025-03-03 20:46:50 -0600

    feat(jeronimo): archivos de la clase

--- clase-03-03-2025/invenrario-ejercicio/index.js
+++ clase-03-03-2025/invenrario-ejercicio/index.js
@@ -2,18 +2,29 @@ const formulario = document.querySelector('form');
 const tabla = document.getElementById('productos');
 
 formulario.addEventListener("submit", (event)=>{
-    event.preventDefault();
     const nombre = formulario.elements['nombre'].value;
     const precio = formulario.elements['precio'].value;
     const descripcion = formulario.elements['descripcion'].value;
     const disponibilidad = formulario.elements['disponibilidad'].value;
     
     const fila = document.createElement("tr");
-    fila.innerHTML += `
+
+    if(disponibilidad <= 15){
+        fila.innerHTML += `
         <td>${nombre}</td>
         <td>${precio}</td>
         <td>${descripcion}</td>
-        <td>${disponibilidad}</td>
+        <td style="color:red;">${disponibilidad}</td>
         `
         tabla.appendChild(fila);
+    }else if(disponibilidad >= 20){
+        fila.innerHTML += `
+        <td>${nombre}</td>
+        <td>${precio}</td>
+        <td>${descripcion}</td>
+        <td style="color:green;">${disponibilidad}</td>
+        `
+        tabla.appendChild(fila);
+    }
+
 });
